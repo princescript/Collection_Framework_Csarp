824c91e269d9bd72c3863a0910ba7ad3b4bf0bac
Prince Singh <princesingh005958@gmail.com>
2026-02-11 16:58:05 +0530

    push

--- Queue_NonGen/Program.cs
+++ Queue_NonGen/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //FIFO = Queue
+        Queue queue = new Queue();
+        queue.Enqueue(1);
+        queue.Enqueue("Ram");
+        queue.Enqueue(4.44);
+        queue.Enqueue(new Program());
+
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("-------");
+
+        //Removing Elements
+        queue.Dequeue();
+
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+
+        //Peek
+        Console.WriteLine(queue.Peek());
+
+        Console.WriteLine("-------");
+
+        //Clone
+        Queue queue1 = (Queue)queue.Clone();
+        if(queue1 != null) Console.WriteLine("queue1 have cloned data");
+
+        //Count
+        queue.Clear();
+        Console.WriteLine(queue.Count);
+
+        //Contains
+        Console.WriteLine(queue.Contains("Ram"));//false
+
+    }
+}
---
+++ SortedList_NonGen/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //ShortedList = key/value pairs
+        SortedList sortedList = new SortedList()
+        {
+            { "Ind", "India" },
+            { "USA", "United States" }
+        };
+        //Adding
+        sortedList.Add("Russia", "Moscow");
+
+        //Clone
+        SortedList cloneList = (SortedList)sortedList.Clone();
+        if(cloneList != null) Console.WriteLine("clone have data");
+
+        //Remove
+        sortedList.Remove("USA");
+        //sortedList.RemoveAt("index");
+        //sortedList.Clear();
+
+        //Checking
+        Console.WriteLine(sortedList.ContainsKey("USA"));
+        Console.WriteLine(sortedList.ContainsValue("Moscow"));
+
+        //
+        foreach (var item in  sortedList)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+}
